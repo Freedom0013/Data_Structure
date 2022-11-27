d37f8f3f9169b07f939d3bed423d0d07ac2d50ec
Freedom0013 <846909723@qq.com>
2022-11-27 13:20:27 +0800

    补充

--- src/com/freedom/datastructure/test/IndexPriorityQueueTest.java
+++ src/com/freedom/datastructure/test/IndexPriorityQueueTest.java
@@ -10,15 +10,18 @@ import com.freedom.datastructure.tree.IndexMinPriorityQueue;
 public class IndexPriorityQueueTest {
     public static void main(String[] args) {
         IndexMinPriorityQueue<String> indexMinPriorityQueue = new IndexMinPriorityQueue<>(10);
-        indexMinPriorityQueue.insert(0,"A");
-        indexMinPriorityQueue.insert(1,"C");
-        indexMinPriorityQueue.insert(2,"F");
-
-        indexMinPriorityQueue.changeItem(2,"B");
-
-        while (!indexMinPriorityQueue.isEmpty()){
-            int i = indexMinPriorityQueue.deleteMin();
-            System.out.print("index:"+ i+" ");
+        indexMinPriorityQueue.insert(0, "A");
+        indexMinPriorityQueue.insert(1, "C");
+        indexMinPriorityQueue.insert(2, "H");
+        indexMinPriorityQueue.insert(3, "F");
+        indexMinPriorityQueue.insert(4, "E");
+        indexMinPriorityQueue.insert(5, "D");
+        indexMinPriorityQueue.changeItem(2, "B");
+        while (!indexMinPriorityQueue.isEmpty()) {
+            int min = indexMinPriorityQueue.getMinIndex();
+            System.out.print("value = " + indexMinPriorityQueue.getItem(min) + " ");
+            int minIndex = indexMinPriorityQueue.deleteMin();
+            System.out.println("index = " + minIndex + "value = " + indexMinPriorityQueue.getItem(min) + " ");
         }
     }
 }
--- src/com/freedom/datastructure/tree/IndexMinPriorityQueue.java
+++ src/com/freedom/datastructure/tree/IndexMinPriorityQueue.java
@@ -19,11 +19,7 @@ import java.util.Arrays;
  * @author Freedom0013 @Date 2022-11-27
  * @version V1.00
  */
-public class IndexMinPriorityQueue<T extends Comparable<T>> {
-    /** 数组存储堆中元素 */
-    private T[] items;
-    /** 堆大小 */
-    private int size;
+public class IndexMinPriorityQueue<T extends Comparable<T>> extends MinPriorityQueue{
     /** 保存每个元素在items中的索引，pq数组需要堆有序 */
     private int[] pq;
     /** 保存pq数组的逆序，使用pq的索引作为qp的元素，pq的元素作为qp的索引 */
@@ -34,20 +30,11 @@ public class IndexMinPriorityQueue<T extends Comparable<T>> {
      * @param capacity 堆大小
      */
     public IndexMinPriorityQueue(int capacity) {
-        this.items = (T[]) new Comparable[capacity + 1];
-        this.size = 0;
+        super(capacity);
         this.pq = new int[capacity + 1];
         this.qp = new int[capacity + 1];
         //默认情况下，qp未存储任何数据，使qp中元素均为-1
         Arrays.fill(qp, -1);
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public boolean isEmpty() {
-        return size == 0;
     }
 
     /**
@@ -56,7 +43,7 @@ public class IndexMinPriorityQueue<T extends Comparable<T>> {
      * @param j j索引
      * @return boolean i是否小于j
      */
-    private boolean less(int i, int j) {
+    protected boolean less(int i, int j) {
         return items[pq[i]].compareTo(items[pq[j]]) < 0;
     }
 
@@ -65,7 +52,7 @@ public class IndexMinPriorityQueue<T extends Comparable<T>> {
      * @param i i索引
      * @param j j索引
      */
-    private void exch(int i, int j) {
+    protected void exch(int i, int j) {
         //交换pq元素
         int temp = pq[i];
         pq[i] = pq[j];
@@ -81,6 +68,9 @@ public class IndexMinPriorityQueue<T extends Comparable<T>> {
      * @return boolean 是否存在
      */
     public boolean contains(int k){
+        if (isEmpty()) {
+            return false;
+        }
         return qp[k] != -1;
     }
 
@@ -89,7 +79,19 @@ public class IndexMinPriorityQueue<T extends Comparable<T>> {
      * @return int 最小元素索引
      */
     public int getMinIndex(){
+        if (isEmpty()) {
+            return -1;
+        }
         return pq[1];
+    }
+
+    /**
+     * 获取i索引处元素值
+     * @param i 索引i
+     * @return T 元素值
+     */
+    public T getItem(int i) {
+        return (T) items[i];
     }
 
     /**
@@ -109,25 +111,13 @@ public class IndexMinPriorityQueue<T extends Comparable<T>> {
     }
 
     /**
-     * 删除最小元素
-     * @return int 最小元素关联的索引
-     */
-    public int deleteMin() {
-        int minIndex = pq[1];   //获取最小元素值索引
-        exch(1, size);      //交换pq处索引1处和最大索引处值等待删除
-        qp[pq[size]] = -1;     //删除qp数组对应pq内容
-        pq[size] = -1;      //删除pq中换下来的最小元素
-        items[minIndex] = null; //删除最小元素items原始值
-        size--;         //元素个数-1
-        sink(1);    //下沉调整刚刚临时换上来的堆顶元素
-        return minIndex;
-    }
-
-    /**
      * 删除i处元素
      * @param i 被删除索引值
      */
     public void delete(int i) {
+        if (isEmpty()) {
+            return;
+        }
         int k = qp[i];  //找到i在pq中的索引
         exch(k, size);   //交换pq中k索引处的值和最大索引的值
         qp[pq[size]] = -1;  //各类删除
@@ -139,45 +129,36 @@ public class IndexMinPriorityQueue<T extends Comparable<T>> {
     }
 
     /**
+     * 删除最小元素
+     * @return int 最小元素关联的索引
+     */
+    public int deleteMin() {
+        if (isEmpty()) {
+            return -1;
+        }
+        int minIndex = pq[1];   //获取最小元素值索引
+        exch(1, size);      //交换pq处索引1处和最大索引处值等待删除
+        qp[pq[size]] = -1;     //删除qp数组对应pq内容
+        pq[size] = -1;      //删除pq中换下来的最小元素
+        items[minIndex] = null; //删除最小元素items原始值
+        size--;         //元素个数-1
+        sink(1);    //下沉调整刚刚临时换上来的堆顶元素
+        return minIndex;
+    }
+
+
+    /**
      * 替换索引i处的值为t
      * @param i 待替换索引值
      * @param t 替换值
      */
     public void changeItem(int i, T t) {
+        if (isEmpty()) {
+            return;
+        }
         items[i] = t;
         int k = qp[i];
         sink(k);        //调整k
         swim(k);
-    }
-
-    private void swim(int k){
-        while (k > 1) {//堆中至少有两个节点时开始比较
-            if (less(k, k / 2)) {//父节点大于当前节点
-                exch(k, k / 2);
-            }
-            k = k / 2;  //找到父节点的父节点继续比较
-        }
-    }
-
-    private void sink(int k){
-        //通过循环不断对比当前k节点和其左子节点2k及右子节点2k+1处的较大值元素，如果当前节点小，则互换位置
-        while (2 * k <= size) {
-            int min;//记录较小节点所在的索引
-            if (2 * k + 1 <= size) {
-                if (less(2 * k, 2 * k + 1)) {
-                    min = 2 * k;
-                } else {
-                    min = 2 * k + 1;
-                }
-            } else {
-                min = 2 * k;
-            }
-            //比较当前节点和较小值，注这里判断与大顶堆区别
-            if (less(k, min)) {
-                break;
-            }
-            exch(k, min);
-            k = min;
-        }
     }
 }
