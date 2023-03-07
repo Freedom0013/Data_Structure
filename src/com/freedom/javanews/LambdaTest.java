4097aabe765e25568e4c9cd789b26b2c27fc1902
Freedom0013 <846909723@qq.com>
2023-03-07 19:03:45 +0800

    fix

--- src/com/freedom/javanews/LambdaTest.java
+++ src/com/freedom/javanews/LambdaTest.java
@@ -1,8 +1,33 @@
 package com.freedom.javanews;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 /**
- * 
- * @author Freedom0013 @Date 2022-12-8 
+ * @author Freedom0013 @Date 2022-12-8
  * @version V1.00
  */
-public class Lambda {
+public class LambdaTest {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("小DDDD");
+        arrayList.add("小BB");
+        arrayList.add("小A");
+        arrayList.add("小CCC");
+
+//        //此实现等价于下方函数式编程语句
+//        arrayList.sort(new Comparator<String>() {
+//            @Override
+//            public int compare(String o1, String o2) {
+//                return o1.length() - o2.length();
+//            }
+//        });
+//        for (String s : arrayList) {
+//            System.out.println(s);
+//        }
+
+        //函数式编程语句stream
+        arrayList.stream().sorted(Comparator.comparingInt(String::length)).forEach(System.out::println);
+    }
 }
